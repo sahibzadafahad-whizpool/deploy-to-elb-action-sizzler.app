70eab57ac27afdffe42a2821096ba8f145c92f68
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2021-11-25 17:01:30 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,5 @@ export IFS=";"
 for instance_id in $INSTANCE_IDS; do \
     INSTANCE_IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $instance_id | jq --raw-output '.Reservations[0].Instances[0].PublicIpAddress'); \
     echo "Deploying to $INSTANCE_IP_ADDRESS..."; \
-    ssh -oStrictHostKeyChecking=no -i /tmp/chillflix.pem chillflix@"$INSTANCE_IP_ADDRESS" $1; \
+    ssh -oStrictHostKeyChecking=no -i /tmp/sizzler.pem sizzler@"$INSTANCE_IP_ADDRESS" $1; \
 done
